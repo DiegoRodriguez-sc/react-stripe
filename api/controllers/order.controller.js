cebead154ae91d0d52cc824d6a930ac28845aa1e
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-02-04 20:28:12 -0300

    get order added

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,19 +1,27 @@
-const Order = require("../models/order")
+const Order = require("../models/order");
 
-
+//TODO: generar una nueva orden
 const postOrder = async (req, res) => {
- try {
-     const { amount, name } = req.body;
-     const order = await Order.create({
-         name,
-         amount
-     });
-     res.status(201).send({ data: oderRes });
- } catch (e) {
-     console.log(e);
-     res.status(500);
-     res.send({ error: 'Algo ocurrio' });
- }
-}
+  try {
+    const { amount, name } = req.body;
+    const order = await Order.create({
+      name,
+      amount,
+    });
+    res.status(201).send({ data: oderRes });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send({ error: "Algo ocurrio" });
+  }
+};
 
-module.exports = { postOrder }
+//TODO: obtener los datos de la orden
+const getOrder = async (req, res) => {
+  const { id } = req.params;
+  const order = await Order.findById(id);
+  res.status(200).send({ data: order });
+};
+
+
+module.exports = { postOrder, getOrder };
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const { postOrder } = require("../controllers/order.controller");
+const { postOrder, getOrder } = require("../controllers/order.controller");
 
 
 const router = Router();
 
 router.post("/", postOrder);
+
+router.get("/:id", getOrder);
 
 module.exports = router;
