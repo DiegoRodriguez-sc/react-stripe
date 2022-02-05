1325f3f57be09b8a2fcbba3b7b6954e33d40158a
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-02-05 03:18:41 -0300

    intencion de pago added y funcionando

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,4 +1,5 @@
 const Order = require("../models/order");
+const { generatePaymentIntent } = require("../services/stripe");
 
 //TODO: generar una nueva orden
 const postOrder = async (req, res) => {
@@ -23,5 +24,36 @@ const getOrder = async (req, res) => {
   res.status(200).send({ data: order });
 };
 
+//TODO: preparamos el pago
+const updateOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { token } = req.body;
 
-module.exports = { postOrder, getOrder };
+    //TODO: Buscamos orden en nuestra base de datos
+
+    const order = await Order.findById(id);
+
+    //TODO: Generamos intencion de pago
+
+    const resPaymentIntent = await generatePaymentIntent({
+      amount: order.amount,
+      user: order.name,
+      payment_method: token,
+    });
+
+    //TODO: Actualizamos  orden con id de intencion de pago
+    console.log(resPaymentIntent);
+    await Order.findByIdAndUpdate(id, {
+      stripeId: resPaymentIntent.id,
+    });
+
+    res.status(201).send({ data: resPaymentIntent });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500);
+    res.send({ error: "Algo ocurrio" });
+  }
+};
+
+module.exports = { postOrder, getOrder, updateOrder };
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { postOrder, getOrder } = require("../controllers/order.controller");
+const { postOrder, getOrder, updateOrder } = require("../controllers/order.controller");
 
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post("/", postOrder);
 
 router.get("/:id", getOrder);
+
+router.put("/:id", updateOrder);
 
 module.exports = router;
